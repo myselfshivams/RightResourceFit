5519c85ab806b6b8b8db8058293d11bd3ff932cd
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-15 18:38:31 +0530

    backend boiler plate

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// ROUTES BELOW
+app.get('/', (req,res)=>{
+  res.send("working");
+});
+
+
+// APP CONFIG START
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
