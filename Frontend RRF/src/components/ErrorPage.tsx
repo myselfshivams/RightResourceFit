80ce88b9fdcfb8837da85cbf5ca61761b90ea0de
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-19 16:47:22 +0530

    fixed:: 404 page with our colour scheme

--- Frontend RRF/src/components/ErrorPage.tsx
+++ Frontend RRF/src/components/ErrorPage.tsx
@@ -1,22 +1,31 @@
-import React from "react";
-import '../styles/errorpage.css'; 
-import { Link } from "react-router-dom";
+import React from 'react';
+import styles from '../styles/NotFound.module.css';
 
-const ErrorPage: React.FC = () => {
+const Custom404: React.FC = () => {
   return (
-    <div className="errorPageMainContainer">
-      <div className="errorMsgContent">
-        <h2 className="error">404</h2>
-        <p>PAGE NOT FOUND</p>
-        <p>It looks like nothing was found at this location.</p>
-      </div>
-      <div className="errorBackToHomeBtn">
-        <Link to="/">
-          <button>BACK TO HOME</button>
-        </Link>
+    <div className={styles.bgg}>
+      <div className={styles.unauthorizedPage}>
+        <div className={styles.leftSection}>
+          <div className={styles.messageContainer}>
+            <h1 className={styles.heading}>404</h1>
+            <p className={styles.subText}>Page Under Construction</p>
+            <p className={styles.instruction}>
+              The page you're trying to access is currently under construction. Please come back later or return to the home page.
+            </p>
+          </div>
+        </div>
+        <div className={styles.rightSection}>
+          <img
+            src="/404.png" 
+            alt="Page Under Construction"
+            className={styles.heroImage}
+            width={500}
+            height={500}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default ErrorPage;
+export default Custom404;
