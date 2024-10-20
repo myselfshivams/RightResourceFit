732c240a50d3fb42b3d5d6413211947d751102a5
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-20 13:42:07 +0530

    update navbar of landing page

--- Frontend RRF/src/pages/Navbar.tsx
+++ Frontend RRF/src/pages/Navbar.tsx
@@ -1,7 +1,17 @@
 import "../styles/Navbar.css"
+import { useNavigate } from 'react-router-dom';
 // import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom"
 const Navbar = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');  // Fetch token from localStorage
+
+  // Handle logout
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    navigate('/login');
+  };
   return (
     <>
       <nav className="navbar">
@@ -11,12 +21,22 @@ const Navbar = () => {
       </div>
       <ul className="navbar-links">
         <Link to="/"><li>Home</li></Link>
-        <Link to="#"><li>About us</li></Link>
+        <Link to="/about"><li>About us</li></Link>
         <Link to="/contact"><li>Contact us</li></Link>
       </ul>
       <div className="Button">
+      {!token ? (
+          <>
         <button><Link className="Login" to="/login">Login</Link></button>
          <button><Link to="/register"> Register</Link></button> 
+         </>
+      ): (
+        <>
+          {localStorage.getItem('role') === 'admin' && <a href="/dashboard">Dashboard</a>}
+          {localStorage.getItem('role') === 'user' && <a href="/user-dashboard">Dashboard</a>}
+          <button className="Login" onClick={handleLogout}>Logout</button>
+        </>
+      )}
       </div>
     </nav>
     </>
