e749d39b7e4931409350be66fb3c1d3f681eeedb
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-21 16:27:39 +0530

    added:: userNotification, userSearch page in User Side view

--- Frontend RRF/src/pages/search.tsx
+++ Frontend RRF/src/pages/search.tsx
@@ -1,25 +1,42 @@
-import React from "react";
-import Sidebar from "../components/UserSidebar";
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Sidebar from '../components/UserSidebar';
+import styles from '../styles/UserSearch.module.css';
 
-const SearchJob: React.FC = () => {
+const Search: React.FC = () => {
+  const [query, setQuery] = useState<string>('');  
+
+  const handleSearch = () => {
+    console.log('Searching for:', query);
+  };
+
   return (
-    <>
-   <Sidebar />
-  
- 
-
-
-   
-
-       
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <h1>Search Page</h1>
+      <div className={styles.SearchContentPage}>
+        <Sidebar />
+        <div className={styles.mainContent}>
+          <h1 className={styles.mainHeading}>Search <span>Users</span></h1>
+          <div className={styles.searchWrapper}>
+            <div className={styles.searchContainer}>
+              <FaSearch className={styles.searchIcon} />
+              <input
+                type="text"
+                placeholder="Search..."
+                className={styles.searchInput}
+                value={query} 
+                onChange={(e) => setQuery(e.target.value)} 
+              />
             </div>
-           
-
-  
-    </>
+            <button
+              className={styles.searchButton}
+              onClick={handleSearch}  
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+ 
   );
-}
+};
 
-export default SearchJob;
+export default Search;
