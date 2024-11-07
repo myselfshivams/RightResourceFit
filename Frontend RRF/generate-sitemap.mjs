9a0a02d9c8c631198fee556313272011870eae9f
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-11-07 14:52:52 +0530

    added:: Meta tags, robots.txt, sitemap.xml to enhance SEO

---
+++ Frontend RRF/generate-sitemap.mjs
@@ -0,0 +1,33 @@
+import { SitemapStream } from 'sitemap';
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pages = [
+  { url: '/', changefreq: 'daily', priority: 1.0 },
+  { url: '/login', changefreq: 'daily', priority: 1.0 },
+  { url: '/register', changefreq: 'daily', priority: 1.0 },
+  { url: '/about', changefreq: 'weekly', priority: 1.0 },
+  { url: '/user-dashboard', changefreq: 'daily', priority: 1.0 },
+  { url: '/user/search', changefreq: 'daily', priority: 1.0 },
+  { url: '/error', changefreq: 'monthly', priority: 1.0 },
+  { url: '/dashboard', changefreq: 'daily', priority: 1.0 },
+   
+  ];
+
+async function generateSitemap() {
+  const writeStream = createWriteStream(path.resolve(__dirname, 'public', 'sitemap.xml'));
+  const sitemap = new SitemapStream({ hostname: 'https://team4.itshivam.me/' });
+  sitemap.pipe(writeStream).on('finish', () => {
+    console.log('Sitemap generated successfully');
+  });
+
+  pages.forEach(page => sitemap.write(page));
+  sitemap.end();
+}
+
+generateSitemap().catch(error => {
+  console.error('Error generating sitemap:', error);
+});
